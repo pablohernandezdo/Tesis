c883b5616bb0c4aa8d338d522c4eb6647fad10eb
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-08 01:52:25 -0400

    Fix model path

--- STEAD_CNN/eval.py
+++ STEAD_CNN/eval.py
@@ -46,7 +46,7 @@ def main():
     net.to(device)
 
     # Load parameters from trained model
-    net.load_state_dict(torch.load('models/' + args.model_name + '.pth'))
+    net.load_state_dict(torch.load('../models/' + args.model_name + '.pth'))
     net.eval()
 
     # Evaluate model on training dataset
