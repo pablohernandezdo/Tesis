8e13a18781a23d96d6df0e6cb04a9b9ab3af007e
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-10 12:21:36 -0400

    Add space

--- STEAD_ANN/exps/XXLeval.sh
+++ STEAD_ANN/exps/XXLeval.sh
@@ -6,6 +6,7 @@ mkdir -p ../models
 trn="Train_data.hdf5"
 tst="Test_data.hdf5"
 
+
 # Classifier_XXL
 echo "Starting evaluation #1"
 python ../eval.py --train_path $trn --test_path $tst --classifier XXL --model_name XXL_lr000001_bs32 > ../logs/XXL_eval/XXL_lr000001_bs32.txt
