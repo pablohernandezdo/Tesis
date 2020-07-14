332d492b77cc7fb87406b332d8508ef834710872
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-13 21:32:02 -0400

    Print hydraulic datashape

--- DAS_Hydraulic/hydraulic_read.py
+++ DAS_Hydraulic/hydraulic_read.py
@@ -24,94 +24,97 @@ def main():
         data = f['data'][()]
         fs = f['fs_f'][()]
 
-    traces = data[:, :6000]
+    print(data.shape)
+    print(fs)
 
-    t_ax = np.arange(1, len(traces[0]) + 1) / fs
-    t_ax = np.squeeze(t_ax)
+    #traces = data[:, :6000]
 
-    trace1 = traces[0]
-    trace2 = traces[450]
-    trace3 = traces[900]
-
-    trace1_fil = butter_bandpass_filter(trace1, 0.1, 10, fs, order=3)
-    trace2_fil = butter_bandpass_filter(trace2, 0.1, 10, fs, order=3)
-    trace3_fil = butter_bandpass_filter(trace3, 0.1, 10, fs, order=3)
-
-    trace1 = trace1 / np.max(np.abs(trace1))
-    trace2 = trace2 / np.max(np.abs(trace2))
-    trace3 = trace3 / np.max(np.abs(trace3))
-
-    trace1_fil = trace1_fil / np.max(np.abs(trace1_fil))
-    trace2_fil = trace2_fil / np.max(np.abs(trace2_fil))
-    trace3_fil = trace3_fil / np.max(np.abs(trace3_fil))
-
-    plt.figure()
-    plt.subplot(311)
-    plt.plot(t_ax, trace1)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-    plt.title('Trazas DAS datos Hydraulic')
-
-    plt.subplot(312)
-    plt.plot(t_ax, trace2)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-
-    plt.subplot(313)
-    plt.plot(t_ax, trace3)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-    plt.tight_layout()
-    plt.savefig('Imgs/TrazasDAS.png')
-
-    plt.clf()
-    plt.subplot(311)
-    plt.plot(t_ax, trace1_fil)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-    plt.title('Trazas DAS datos HYdraulic filtrados 1 - 10 Hz')
-
-    plt.subplot(312)
-    plt.plot(t_ax, trace2_fil)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-
-    plt.subplot(313)
-    plt.plot(t_ax, trace3_fil)
-    plt.grid(True)
-    plt.ylabel('Strain [-]')
-    plt.xlabel('Tiempo [s]')
-    plt.tight_layout()
-    plt.savefig('Imgs/TrazasDAS_fil.png')
-
-    plt.clf()
-    line_st, = plt.plot(trace1, label='DAS')
-    line_das, = plt.plot(st_trace, label='STEAD')
-    plt.grid(True)
-    plt.xlabel('Muestras [-]')
-    plt.ylabel('Strain [-]')
-    plt.title('Traza STEAD y traza DAS HYdraulic')
-    plt.legend(handles=[line_st, line_das], loc='upper left')
-    plt.savefig('Imgs/STEADHYdraulic.png')
-
-    plt.clf()
-    plt.subplot(211)
-    plt.plot(st_trace)
-    plt.grid(True)
-    plt.xlabel('Muestras [-]')
-    plt.ylabel('Strain [-]')
-    plt.title('Traza STEAD y traza DAS HYdraulic')
-    plt.subplot(212)
-    plt.plot(trace1)
-    plt.grid(True)
-    plt.xlabel('Muestras [-]')
-    plt.ylabel('Strain [-]')
-    plt.savefig('Imgs/STEADHYdraulic1.png')
+    # t_ax = np.arange(1, len(traces[0]) + 1) / fs
+    # t_ax = np.squeeze(t_ax)
+    #
+    # trace1 = traces[0]
+    # trace2 = traces[450]
+    # trace3 = traces[900]
+    #
+    # trace1_fil = butter_bandpass_filter(trace1, 0.1, 10, fs, order=3)
+    # trace2_fil = butter_bandpass_filter(trace2, 0.1, 10, fs, order=3)
+    # trace3_fil = butter_bandpass_filter(trace3, 0.1, 10, fs, order=3)
+    #
+    # trace1 = trace1 / np.max(np.abs(trace1))
+    # trace2 = trace2 / np.max(np.abs(trace2))
+    # trace3 = trace3 / np.max(np.abs(trace3))
+    #
+    # trace1_fil = trace1_fil / np.max(np.abs(trace1_fil))
+    # trace2_fil = trace2_fil / np.max(np.abs(trace2_fil))
+    # trace3_fil = trace3_fil / np.max(np.abs(trace3_fil))
+    #
+    # plt.figure()
+    # plt.subplot(311)
+    # plt.plot(t_ax, trace1)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    # plt.title('Trazas DAS datos Hydraulic')
+    #
+    # plt.subplot(312)
+    # plt.plot(t_ax, trace2)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    #
+    # plt.subplot(313)
+    # plt.plot(t_ax, trace3)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    # plt.tight_layout()
+    # plt.savefig('Imgs/TrazasDAS.png')
+    #
+    # plt.clf()
+    # plt.subplot(311)
+    # plt.plot(t_ax, trace1_fil)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    # plt.title('Trazas DAS datos HYdraulic filtrados 1 - 10 Hz')
+    #
+    # plt.subplot(312)
+    # plt.plot(t_ax, trace2_fil)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    #
+    # plt.subplot(313)
+    # plt.plot(t_ax, trace3_fil)
+    # plt.grid(True)
+    # plt.ylabel('Strain [-]')
+    # plt.xlabel('Tiempo [s]')
+    # plt.tight_layout()
+    # plt.savefig('Imgs/TrazasDAS_fil.png')
+    #
+    # plt.clf()
+    # line_st, = plt.plot(trace1, label='DAS')
+    # line_das, = plt.plot(st_trace, label='STEAD')
+    # plt.grid(True)
+    # plt.xlabel('Muestras [-]')
+    # plt.ylabel('Strain [-]')
+    # plt.title('Traza STEAD y traza DAS HYdraulic')
+    # plt.legend(handles=[line_st, line_das], loc='upper left')
+    # plt.savefig('Imgs/STEADHYdraulic.png')
+    #
+    # plt.clf()
+    # plt.subplot(211)
+    # plt.plot(st_trace)
+    # plt.grid(True)
+    # plt.xlabel('Muestras [-]')
+    # plt.ylabel('Strain [-]')
+    # plt.title('Traza STEAD y traza DAS HYdraulic')
+    # plt.subplot(212)
+    # plt.plot(trace1)
+    # plt.grid(True)
+    # plt.xlabel('Muestras [-]')
+    # plt.ylabel('Strain [-]')
+    # plt.savefig('Imgs/STEADHYdraulic1.png')
 
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
