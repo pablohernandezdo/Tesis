7a8d20f05f2128579aaf178da7f933f25844eaeb
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-15 15:14:30 -0400

    Print trtp len

--- DAS_STEAD/STEAD_read.py
+++ DAS_STEAD/STEAD_read.py
@@ -16,23 +16,28 @@ def main():
 
     Path("Imgs").mkdir(exist_ok=True)
 
-    # Carga traza STEAD
-
+    # STEAD dataset path
     st = '../Data_STEAD/Train_data.hdf5'
 
+    # Number of traces to plot
+    n = 4
+
+    # Traces to plot
+    trtp = []
+
     with h5py.File(st, 'r') as h5_file:
+
+        # Seismic traces group
         grp = h5_file['earthquake']['local']
 
-        # Traces to plot
-        trtp = random.randint(0, high=len(grp), size=(1, 3))
+        # Traces to plot ids
+        trtp_ids = random.randint(0, high=len(grp), size=(1, n))
 
-        print(trtp)
-        # for idx, dts in enumerate(grp):
-        #     if id == idx:
-        #         st_trace = grp[dts][:, 0] / np.max(np.abs(grp[dts][:, 0]))
-        #         break
-        #     else:
-        #         continue
+        for idx, dts in enumerate(grp):
+            if idx in trtp_ids:
+                trtp.append(grp[dts][:, 0])
+
+    print(len(trtp))
 
     # plt.figure()
     # plt.plot(st_trace)
