87fcb8b5eea9dc01342a0cf9c99b741f543d1f72
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-15 12:57:37 -0400

    Don't normalize

--- DAS_Reykjanes/reykjanes_read.py
+++ DAS_Reykjanes/reykjanes_read.py
@@ -182,7 +182,7 @@ def main():
                 data['strain'] = np.concatenate((data['strain'], np.expand_dims(row, 0)))
 
     data['strain'] = data['strain'][1:]
-    data['strain'] = data['strain'] / data['strain'].max(axis=0)
+    # data['strain'] = data['strain'] / data['strain'].max(axis=0)
     data['strain'] = data['strain'].transpose()
     data_das = data
 
