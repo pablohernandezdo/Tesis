74e7787ee74ea095975d08dd417793b8fda08dd7
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-17 18:29:21 -0400

    Test sort

--- Senales/xxx.py
+++ Senales/xxx.py
@@ -2,7 +2,7 @@ from numpy.random import default_rng
 
 def main():
     rng = default_rng()
-    numbers = rng.choice(20, size=10, replace=False)
+    numbers = rng.choice(20, size=10, replace=False).sort()
     print(numbers)
 
 if __name__ == "__main__":
