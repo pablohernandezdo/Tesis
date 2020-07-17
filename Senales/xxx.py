15a1f6b85cc9c15567d2e773db61e8611a2a566a
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-17 19:03:30 -0400

    Delete test file

--- Senales/xxx.py
+++
@@ -1,9 +0,0 @@
-from numpy.random import default_rng
-
-def main():
-    rng = default_rng()
-    numbers = rng.choice(20, size=10, replace=False).sort()
-    print(numbers)
-
-if __name__ == "__main__":
-    main()
