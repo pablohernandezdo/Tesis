14a00a1ace33ae4cdaa750fa2a198d6b27b53296
Pablo <pablo.hernandezdo.13@sansano.usm.cl>
2020-07-21 13:47:45 -0400

    Build test dataset

---
+++ Dataset_Prueba/dataset_prueba.py
@@ -0,0 +1,39 @@
+import re
+import h5py
+import numpy as np
+
+import scipy.signal as signal
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+from pathlib import Path
+from scipy.signal import butter, lfilter
+
+
+def main():
+    # Dataset Reykjanes
+
+    # Telesismo Fibra optica
+
+    file_fo = '../Data_Reykjanes/Jousset_et_al_2018_003_Figure3_fo.ascii'
+
+    fs = 20
+
+    data_fo = {
+        'head': '',
+        'strain': []
+    }
+
+    with open(file_fo, 'r') as f:
+        for idx, line in enumerate(f):
+            if idx == 0:
+                data_fo['head'] = line.strip()
+            else:
+                val = line.strip()
+                data_fo['strain'].append(float(val))
+
+    print(np.asarray(data_fo['strain']).shape)
+
+if __name__ == "__main__":
+    main()
